786082bb6b0969a89196dfef0a98bdbf2867bfcd
Juan C. Moreno B <juan.carlos.moreno.banda@hotmail.com>
2022-02-15 15:52:42 -0500

    add route profile

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Navigation from "./components/navbar";
 import Home from "./pages/home";
 import Modalsignin from "./components/modalsignin";
 import Modalsignup from "./components/modalsignup";
+import Profile from "./pages/profile";
 
 function App() {
   return (
     <>
       <Navigation />
-      <Home />
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/modalsignin" element={<Modalsignin />} />
         <Route path="/modalsignup" element={<Modalsignup />} />
       </Routes>
