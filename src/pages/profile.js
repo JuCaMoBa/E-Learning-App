66b50e50ddfe1921a437fb83eb5cb0655921a774
Juan C. Moreno B <juan.carlos.moreno.banda@hotmail.com>
2022-02-15 14:25:42 -0500

    add profile page

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,5 +1,32 @@
 import React from "react";
+import Maths from "../img/Math.png";
+import Enginering from "../img/enginering.png";
+import Science from "../img/Science.png";
 
-export default function Profile() {
-  return <div>profile</div>;
+import "../styles/profile.scss";
+
+export default function Home() {
+  return (
+    <div className="container_primary">
+      <div className="container_secondary">
+        <div className="container_item_1">
+          <h1>Easy Maths</h1>
+        </div>
+      </div>
+      <div className="container_tertiary">
+        <div className="container_item_2">
+          <img src={Maths} alt="Logo-Elearning" />
+          <h3>Maths 4-8</h3>
+        </div>
+        <div className="container_item_3">
+          <img src={Enginering} alt="Logo-Elearning" />
+          <h3>Maths 9-14</h3>
+        </div>
+        <div className="container_item_4">
+          <img src={Science} alt="Logo-Elearning" />
+          <h3>Maths 15+</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
