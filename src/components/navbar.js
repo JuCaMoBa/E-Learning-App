543fdbc02a59bf5f6aec647fd89ab64215c2d97a
Juan C. Moreno B <juan.carlos.moreno.banda@hotmail.com>
2022-02-16 11:22:43 -0500

    add navbar when user log in

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import UserContext from "../store/context";
+import { types } from "../types/types";
+import iconImage from "../img/circleImage.png";
 import "../styles/navbar.scss";
 
 function Navigation() {
+  const { state, dispatch } = useContext(UserContext);
+  const navigate = useNavigate();
   function toggle() {
     var x = document.getElementById("topnav");
     if (x.className === "navbar") {
@@ -13,7 +18,12 @@ function Navigation() {
     console.log(x.className);
   }
 
-  return (
+  function onSignOut() {
+    dispatch({ type: types.signout });
+    navigate("/signin");
+  }
+
+  return !state.isLoggedIn ? (
     <div className="container">
       <div className="navbar" id="topnav">
         <Link className="link_home" to="/">
@@ -30,6 +40,23 @@ function Navigation() {
         <div className="icon_bar"></div>
         <div className="icon_bar"></div>
         <div className="icon_bar"></div>
+      </div>
+    </div>
+  ) : (
+    <div className="container">
+      <div className="navbar" id="topnav">
+        <Link className="link_home" to="/">
+          HOME
+        </Link>
+        <Link className="link_singin" to="/profile">
+          PROFILE
+        </Link>
+        <Link onClick={onSignOut} className="link_getstarted" to="/">
+          SING OUT
+        </Link>
+      </div>
+      <div className="icon" onClick={toggle}>
+        <img src={iconImage} alt="" className="icon__profile" />
       </div>
     </div>
   );
