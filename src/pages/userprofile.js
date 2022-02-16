25b16f8d3f4099f5cf24b0fc1a56dfb1367e58f0
Juan C. Moreno B <juan.carlos.moreno.banda@hotmail.com>
2022-02-16 14:32:47 -0500

    add initial page profile

--- src/pages/userprofile.js
+++ src/pages/userprofile.js
@@ -1,5 +1,20 @@
 import React from "react";
+import "../styles/userprofile.scss";
+import iconImage from "../img/circleImage.png";
 
 export default function Userprofile() {
-  return <div>userprofile</div>;
+  return (
+    <div className="user">
+      <div className="user__info">
+        <h4 className="user__name">Pedro Perez</h4>
+        <img className="user__image" src={iconImage} alt="" />
+        <h6 className="user__email">Email:</h6>
+        <h6 className="user__phone">Phone:</h6>
+        <h6 className="user__country">Country:</h6>
+        <div>
+          <button className="user__editprofile">Edit Profile</button>
+        </div>
+      </div>
+    </div>
+  );
 }
