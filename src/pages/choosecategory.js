18a1079160c3b90fcd49053da5ae7a2d78ca63c1
Juan C. Moreno B <juan.carlos.moreno.banda@hotmail.com>
2022-02-22 15:52:12 -0500

    add to contextProvider the Category

--- src/pages/choosecategory.js
+++ src/pages/choosecategory.js
@@ -1,8 +1,9 @@
 import React, { useContext } from "react";
 import Maths from "../img/Math.png";
 import { useNavigate } from "react-router-dom";
-import Enginering from "../img/enginering.png";
+import history from "../img/history.png";
 import Science from "../img/Science.png";
+import geography from "../img/geography.png";
 import { types } from "../types/types";
 import UserContext from "../store/context";
 
@@ -14,14 +15,33 @@ export default function Choosecategory() {
   function goMathlevel() {
     return navigate("/mathslevel");
   }
-  function goTrivia() {
+  function goTriviaHistory() {
     dispatch({
       type: types.trivia,
       payload: {
         category: "history",
       },
     });
+    return navigate("/trivia");
+  }
 
+  function goTriviaGeography() {
+    dispatch({
+      type: types.trivia,
+      payload: {
+        category: "geography",
+      },
+    });
+    return navigate("/trivia");
+  }
+
+  function goTriviaScience() {
+    dispatch({
+      type: types.trivia,
+      payload: {
+        category: "science",
+      },
+    });
     return navigate("/trivia");
   }
 
@@ -38,12 +58,20 @@ export default function Choosecategory() {
           <h3>Maths</h3>
         </div>
         <div className="container_item_3">
-          <img onClick={goTrivia} src={Enginering} alt="Logo-Elearning" />
+          <img onClick={goTriviaHistory} src={history} alt="Logo-Elearning" />
           <h3>History</h3>
         </div>
         <div className="container_item_4">
-          <img src={Science} alt="Logo-Elearning" />
+          <img onClick={goTriviaScience} src={Science} alt="Logo-Elearning" />
           <h3>Science</h3>
+        </div>
+        <div className="container_item_5">
+          <img
+            onClick={goTriviaGeography}
+            src={geography}
+            alt="Logo-Elearning"
+          />
+          <h3>Geography</h3>
         </div>
       </div>
     </div>
