8fd24c970355b28062c7b1a38acdeb692f27d5b7
Juan C. Moreno B <juan.carlos.moreno.banda@hotmail.com>
2022-02-23 09:04:04 -0500

    delete console.logs

--- src/components/formsignup.js
+++ src/components/formsignup.js
@@ -30,7 +30,6 @@ export default function Formsignup() {
 
   const formSubmit = async (dataForm) => {
     const { data } = await userSignUp({ ...dataForm });
-    console.log(dataForm);
     if (data) {
       dispatch({
         type: types.signup,
